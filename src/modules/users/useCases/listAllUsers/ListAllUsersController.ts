19ea6a4c2c145a1a5908ceb91ffb7556e7e9463a
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-08-26 10:48:01 -0300

    feat: add list users use case

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,13 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    const all = this.listAllUsersUseCase.execute({ user_id } as {
+      user_id: string;
+    });
+
+    return response.json(all);
   }
 }
 
